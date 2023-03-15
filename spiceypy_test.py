0bef74ac20b3ec14b16fe69e5d96f364022edddb
Shin <shinnosuke.satoh@pparc.gp.tohoku.ac.jp>
2023-03-15 13:49:27 +0900

    Spicepy installed.

---
+++ spiceypy_test.py
@@ -0,0 +1,18 @@
+""" spiceypy_test.py
+
+Created on Mar 15, 2023
+@author: Shin Satoh
+
+Description:
+
+
+Version
+1.0.0 (Mar 15, 2023) Determination of Jupiter's coordinate
+
+"""
+
+# %% Importing libraries
+import numpy as np
+import spiceypy as spice
+
+print(spice.tkvrsn('TOOLKIT'))
