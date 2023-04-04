ef5bbf5b865926c36da1ed25cfa07b46223fc5a2
Shin <shinnosuke.satoh@pparc.gp.tohoku.ac.jp>
2023-04-04 15:32:37 +0900

    Sys3 periodicity modified.

--- eu_ftpS3.py
+++ eu_ftpS3.py
@@ -1,3 +1,16 @@
+""" eu_ftpS3.py
+
+Created on Mar 15, 2023
+@author: Shin Satoh
+
+Description:
+
+
+Version
+1.0.0 (Mar 15, 2023)
+
+"""
+
 import spiceypy as spice
 import numpy as np
 
@@ -34,20 +47,35 @@ class ftpS3():
         print('R', posr/71492, ', PHI', np.degrees(posphi), ', SYS3', Sys3)
 
         # Search the System III index
-        s3list = np.arange(0, 360, 5)
+        s3list = np.arange(0, 361, 5)
         argsorted = np.argsort(np.abs(s3list-Sys3), axis=0)
         s3_idx0, s3_idx1 = argsorted[0], argsorted[1]
 
         EUs3 = [s3list[s3_idx0], s3list[s3_idx1]]
+        print('EUS3', EUs3)     # [350, 355]のときに計算が狂う
 
         s3wlon = [satmodel.euwlon[s3_idx0], satmodel.euwlon[s3_idx1]]
         s3lat = [satmodel.eulat[s3_idx0], satmodel.eulat[s3_idx1]]
+        print('s3wlon', s3wlon)
+        print('s3lat', s3lat)
 
-        s3wlon_lin = (
-            (s3wlon[1]-s3wlon[0])/(EUs3[1]-EUs3[0]))*(Sys3-EUs3[0]) + s3wlon[0]
+        if (s3wlon[0]-s3wlon[1]) > 300:
+            ds3wlon10 = (s3wlon[1]+360)-s3wlon[0]
+            y0 = s3wlon[0]
+        elif (s3wlon[1]-s3wlon[0]) > 300:
+            ds3wlon10 = s3wlon[1]-(s3wlon[0]+360)
+            y0 = s3wlon[0]+360
+        else:
+            ds3wlon10 = s3wlon[1]-s3wlon[0]
+            y0 = s3wlon[0]
+
+        s3wlon_lin = (ds3wlon10/(EUs3[1]-EUs3[0]))*(Sys3-EUs3[0]) + y0
         s3lat_lin = (
             (s3lat[1]-s3lat[0])/(EUs3[1]-EUs3[0]))*(Sys3-EUs3[0]) + s3lat[0]
 
-        print('EU FTP LAT', s3lat_lin, ', SYS3', s3wlon_lin)
+        if s3wlon_lin > 360:
+            s3wlon_lin += -360
+
+        print('EFP LAT', s3lat_lin, ', SYS3', s3wlon_lin)
 
         return s3wlon_lin, s3lat_lin
